ff1dcaa3c6f3c20970f99e4576de6a278d25de5a
elissonmaster <imelissonmoreira@gmail.com>
2025-01-28 19:52:20 -0300

    commit

--- menu.js
+++ menu.js
@@ -10,7 +10,7 @@ window.addEventListener("scroll", function () {
 let count = 1;
 document.getElementById("radio1").checked = true;
 
-setInterval (function() { nextImage();},6000)
+setInterval (function() { nextImage();},4000)
 
 
 function nextImage(){ count++; if(count>4){ count = 1;}
---
+++ reveal.js
@@ -0,0 +1,16 @@
+
+window.revelar = ScrollReveal ({reset:true})
+
+//TOPO DO SITE
+
+revelar.reveal('.efeitotxt', 
+    {
+     duration: 2000,
+     distance: '90px'
+})
+
+revelar.reveal('.efeitoimg', 
+    {
+     duration: 2000,
+     distance: '90px'
+})
